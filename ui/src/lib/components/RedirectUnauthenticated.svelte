7618eeacbd9c13ac6beda368cdd9dafe2309629a
John Roberts <john.calvin.roberts@gmail.com>
2023-04-03 15:05:16 -0600

    feat: redirect unauthenticated component

---
+++ ui/src/lib/components/RedirectUnauthenticated.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	/* eslint-disable import/no-unresolved */
+	import { apiClient } from "../stores/api";
+	import { goto } from "$app/navigation";
+	/* eslint-enable import/no-unresolved */
+
+	const { store } = apiClient;
+	$: {
+		if (!$store.isAuthenticated) {
+			goto("/");
+		}
+	}
+</script>
+
+<slot />
