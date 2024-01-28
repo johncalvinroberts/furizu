4a303ac9ecca3557a4a1413c55f569d72fe05705
John Roberts <john.calvin.roberts@gmail.com>
2024-01-28 14:39:34 -0500

    chore: scaffold file detail

---
+++ ui/src/lib/components/pages/File.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import RedirectUnauthenticated from "../RedirectUnauthenticated.svelte";
+	// Your script here
+</script>
+
+<RedirectUnauthenticated>stuff here</RedirectUnauthenticated>
+
+<style>
+	/* Your styles here */
+</style>
---
+++ ui/src/routes/files/[key]/+page.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+	import File from "../../../lib/components/pages/File.svelte";
+</script>
+
+<File />
