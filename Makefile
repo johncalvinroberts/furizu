325600b7b799d321f4db131db0121f049e80c892
John Roberts <john.calvin.roberts@gmail.com>
2024-03-13 23:13:37 -0400

    web base

---
+++ Makefile
@@ -0,0 +1,23 @@
+FE_DIR=web
+BE_DIR=server
+
+help:
+	@grep -E '^[a-zA-Z_-]+:.*?## .*$$' $(MAKEFILE_LIST) | sort | awk 'BEGIN {FS = ":.*?## "}; {printf "\033[36m%-30s\033[0m %s\n", $$1, $$2}'
+
+build: ## build everything
+	make -j 2 build-fe build-be
+
+dev: ## run dev servers of both backend and frontend
+	make -j 2 dev-fe dev-be
+
+dev-be: ## run backend dev server with hot reload
+	cd $(BE_DIR); npm run dev;
+
+dev-fe: ## run frontend dev vite server with hot reload
+	cd $(FE_DIR); npm run dev;
+
+build-fe: ## run frontend dev vite server with hot reload
+	cd $(FE_DIR); npm run build;
+
+build-be: ## run frontend dev vite server with hot reload
+	cd $(BE_DIR); npm run build;
---
+++ web/src/App.tsx
@@ -0,0 +1,28 @@
+import './App.css';
+
+import { useState } from 'react';
+
+import reactLogo from './assets/react.svg';
+import { API_URL } from './config';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div>
+        <a href="https://react.dev" target="_blank" rel="noreferrer">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
+        <p>API_URL: {API_URL}</p>
+      </div>
+      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+    </>
+  );
+}
+
+export default App;
