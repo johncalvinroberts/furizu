5e6a64d0afc6f1a8a16aefe823a03fd449b519f6
John Roberts <john.calvin.roberts@gmail.com>
2024-03-14 23:04:00 -0400

    remove panda/ark/park

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,13 +1,9 @@
-import { css } from '../styled-system/css';
-import { Button } from './components/button';
 import { API_URL } from './config';
 
 function App() {
   return (
     <>
-      <div className={css({ fontSize: '2xl', fontWeight: 'bold' })}>Hello 🐼!</div>
       <p>API_URL: {API_URL}</p>
-      <Button>yolo</Button>
     </>
   );
 }
